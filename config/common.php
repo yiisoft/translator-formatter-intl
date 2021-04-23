83c3ba756a17424d358937a7d5460fddffae39da
DarkDef <darkdef@gmail.com>
2021-04-23 22:36:51 +0300

    Add config

---
+++ config/common.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+use Yiisoft\Translator\MessageFormatterInterface;
+use Yiisoft\Translator\Formatter\Intl\IntlMessageFormatter;
+
+return [
+    MessageFormatterInterface::class => IntlMessageFormatter::class,
+];
